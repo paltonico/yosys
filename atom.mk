e393babfb55507a047bbf90a2610930148d9c388
Juan R. <contact@zekemyapp.com>
2025-11-30 14:16:39 +0100

    Fix atom build

--- atom.mk
+++ atom.mk
@@ -6,20 +6,26 @@ LOCAL_MODULE := yosys
 LOCAL_CATEGORY_PATH := tools
 LOCAL_DESCRIPTION := Yosys Open SYnthesis Suite.
 
-LOCAL_ARCHIVE_VERSION := 0.59.1
-LOCAL_ARCHIVE := $(LOCAL_MODULE)-$(LOCAL_ARCHIVE_VERSION).tar.gz
-LOCAL_ARCHIVE_SUBDIR := $(LOCAL_MODULE)-$(LOCAL_ARCHIVE_VERSION)
-
 YOSYS_BUILD_DIR := $(call local-get-build-dir)
+YOSYS_SOURCE_DIR := $(LOCAL_PATH)
 
 YOSYS_MAKE_ARGS := \
-	-C $(YOSYS_BUILD_DIR)/$(LOCAL_ARCHIVE_SUBDIR)
+	-C $(YOSYS_BUILD_DIR) \
+	-f $(LOCAL_PATH)/Makefile \
+	PREFIX=$(TARGET_OUT_STAGING)
 
 # Build step
 define LOCAL_CMD_BUILD
 	@echo "Building yosys"
 	$(MAKE) $(YOSYS_MAKE_ARGS)
 	@echo "yosys built"
+endef
+
+# Build install
+define LOCAL_CMD_BUILD
+	@echo "Installing yosys"
+	$(MAKE) install $(YOSYS_MAKE_ARGS)
+	@echo "yosys installed"
 endef
 
 include $(BUILD_CUSTOM)
